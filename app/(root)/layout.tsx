94825df3e1edc6c8249db66feaa6ac17e5ba4228
ayar <ayarlwinmaw@gmail.com>
2026-09-23 01:13:24 -0700

    header component added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,9 @@
+import Header from "@/components/shared/header";
+
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
         <div className="flex h-screen flex-col">
+            <Header></Header>
             <main className="flex-1 wrapper">
                 {children}
             </main>
