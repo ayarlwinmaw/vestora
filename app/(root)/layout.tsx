5a5218a30568e2f30cab90fb86f2de2f05822cb7
ayar <ayarlwinmaw@gmail.com>
2026-09-23 01:50:12 -0700

    header and footer added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/footer";
 import Header from "@/components/shared/header";
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
@@ -7,6 +8,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
             <main className="flex-1 wrapper">
                 {children}
             </main>
+            <Footer />
         </div>
   );
 }
